7eb8fafc0a33274ead258446bcba2e433dd54315
calgarabide <cristianalgarabidemarin@yahoo.es>
2016-05-18 14:08:29 +0200

    Tema 10 lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,37 +1,37 @@
 var models = require('../models');
 
-// GET /question
+// GET/quizzes
+exports.index = function(req, res, next){
+	models.Quiz.findAll().then(function(quizzes){
+		res.render('quizzes/index.ejs', {quizzes: quizzes});
+	}).catch(function(error) { next(error); });
+
+}
+
+// GET /quizzes/:id
 exports.show = function(req, res, next){
 	models.Quiz.findById(req.params.quizId).then(function(quiz) {
 		if (quiz) {
-			var answer = req.query.answer || '';
+			var answer = req.query.answer || "";
 			res.render('quizzes/show', {quiz: quiz, answer: answer});
 		}
 		else{
-			throw new Error('No hay preguntas en la BBDD.');
+			throw new Error('No existe ese quiz en la BBDD.');
 		}
 	}).catch(function(error) { next(error);});
 };
 
-// GET /check
+// GET /quizzes/:id/check
 exports.check = function(req, res) {
 	models.Quiz.findById(req.params.quizId).then(function(quiz) {
 		if (quiz) {
-			var answer = req.query.answer || "";
+			var answer = req.query.answer || '';
 			var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
 			res.render('quizzes/result', { quiz: quiz, result: result, answer: answer });
 		}
 		else{
-			throw new Error('No hay preguntas en la BBDD.');
+			throw new Error('No existe ese quiz en la BBDD.');
 		}
 	}).catch(function(error) { next(error); });
 };
-
-// GET /quizzes
-exports.index = function(req, res, next){
-	models.Quiz.findAll().then(function(quizzes) { var answer = req.query.answer || ''; 
-		res.render('quizzes/index.ejs', {quizzes : quizzes});
-	}).catch(function(error) { next(error); });
-};
-
 
