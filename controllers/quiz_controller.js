12a8ad24f2affaf46e3a1ed49fd3a26b44ddeddd
calgarabide <cristianalgarabidemarin@yahoo.es>
2016-05-22 17:02:38 +0200

    Tema 12: crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,6 +12,25 @@ exports.load = function(req, res, next, quizId){
 	}).catch(function(error) { next(error);});
 };
 
+// GET /quizzes/new
+exports.new = function(req, res, next){
+	var quiz = models.Quiz.build({question: "", answer: ""});
+	res.render('quizzes/new', {quiz: quiz});
+};
+
+// POST /quizzes/create
+exports.create = function(req, res, next){
+	var quiz = models.Quiz.build({ question: req.body.quiz.question, answer: req.body.quiz.answer});
+
+// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["question", "answer"]}).then(function(quiz){
+		res.redirect('/quizzes'); //res.redirect:
+	}).catch(function(error){
+		next(error);
+	});
+};
+
+
 // GET/quizzes
 exports.index = function(req, res, next){
 		if(req.query.search === undefined){
