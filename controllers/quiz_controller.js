d810242936e8d99f6f8c0b80d9f842373435ba31
calgarabide <cristianalgarabidemarin@yahoo.es>
2016-05-23 22:52:22 +0200

    Tema 14

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,5 @@
 var models = require('../models');
+var Sequelize = require('sequelize');
 
 //Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId){
@@ -26,6 +27,12 @@ exports.create = function(req, res, next){
 	quiz.save({fields: ["question", "answer"]}).then(function(quiz){
 		req.flash('success', 'Quiz creado con éxito.');
 		res.redirect('/quizzes'); //res.redirect:
+	}).catch(Sequelize.ValidationError, function(error) { 
+		req.flash('error', 'Errores en el formulario:');
+		for (var i in error.errors) {
+		req.flash('error', error.errors[i].value);
+		};
+		res.render('quizzes/new', {quiz: quiz});
 	}).catch(function(error){
 		req.flash('error', 'Error al crear un Quiz: '+error.message);
 		next(error);
