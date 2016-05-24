93438b12af3b08ff265790efb65a35d42ec30dfc
calgarabide <cristianalgarabidemarin@yahoo.es>
2016-05-24 15:50:06 +0200

    Tema 17

--- models/index.js
+++ models/index.js
@@ -4,9 +4,9 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
    // DATABASE_URL = "sqlite:///"
-DATABASE_STORAGE = "quiz.sqlite"
+//DATABASE_STORAGE = "quiz.sqlite"
  // Usar BBDD Postgres:
-DATABASE_URL = "postgres://xrpqjupolbysop:owiXEDLT72YgAuMoGnl-uDD_fA@ec2-50-19-242-27.compute-1.amazonaws.com:5432/dbm92297nqfv7q";
+//DATABASE_URL = "postgres://xrpqjupolbysop:owiXEDLT72YgAuMoGnl-uDD_fA@ec2-50-19-242-27.compute-1.amazonaws.com:5432/dbm92297nqfv7q";
 
 var url = DATABASE_URL;
 var storage = DATABASE_STORAGE || "";
@@ -26,20 +26,5 @@ var sequelize = new Sequelize(url, { storage: storage, omitNull: true });
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().then(function(){ // sync() crea la tabla quiz
-  return Quiz.count().then(function (c) {
-      if (c === 0){ // la tabla se inicializa si está vacía
-        return Quiz.bulkCreate([{question: 'Capital de Italia', answer: 'Roma'},
-{question: 'Capital de Portugal', answer: 'Lisboa' }]).then(function() {
-            console.log('Base de datos inicializada con datos');
-          });
-      }
-    });
-}).catch(function(error) {
-    console.log("Error Sincronizando las tablas de la BBDD:", error);
-    process.exit(1);
-});
 
 exports.Quiz = Quiz; //exportar definición de tabla Quiz    
