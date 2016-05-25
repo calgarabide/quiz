f2ac5567ed99de6996fe0b9378d04b5e348a0c33
calgarabide <cristianalgarabidemarin@yahoo.es>
2016-05-25 15:24:51 +0200

    Tema 20

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,8 +10,9 @@ router.get('/', function(req, res, next) {
 });
 
 
-// Autoload de rutas que usen :quizId
+// Autoload de parámetros
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('userId', userController.load); // autoload :userId
 
 // Definición de rutas de /quizzes
 router.get('/quizzes.:format?', 			   quizController.index);
@@ -23,5 +25,14 @@ router.put('/quizzes/:quizId(\\d+)',	   quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',	   quizController.destroy);
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', commentController.create);
+
+// Definición de rutas de cuenta
+router.get('/users', 			userController.index);	// listado usuarios
+router.get('/users/:userId(\\d+)', userController.show);	// ver un usuario
+router.get('/users/new', 		userController.new);	// formulario sign un
+router.post('/users', 			userController.create);// registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit);	// editar cuenta
+router.put('/users/:userId(\\d+)', userController.update);	// actualizar cuenta
+router.delete('/users/:userId(\\d+)', userController.destroy);// borrar cuenta
 
 module.exports = router;
